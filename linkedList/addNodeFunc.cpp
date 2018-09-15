e22a97bd4f594550abacee85131474c2924a35e4
sushishk <kumarsushish51@gmail.com>
2018-09-15 15:49:27 +0530

    add node insertion

---
+++ linkedList/addNodeFunc.cpp
@@ -0,0 +1,14 @@
+void push(struct Node** head_ref, int new_data) 
+{ 
+    /* 1. allocate node */
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+   
+    /* 2. put in the data  */
+    new_node->data  = new_data; 
+   
+    /* 3. Make next of new node as head */
+    new_node->next = (*head_ref); 
+   
+    /* 4. move the head to point to the new node */
+    (*head_ref)    = new_node; 
+} 
